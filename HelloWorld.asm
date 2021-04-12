01d702b58bf5fdccfa07aa3c600817fb10c92b48
tldmschl81 <chse0810@gmail.com>
2021-04-12 18:17:47 +0900

    1-시프트연산과 논리연산

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -5,73 +5,61 @@ global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
    
-    GET_DEC 1, al ;10진수로 input
-    GET_DEC 1, num
-    
-    ;PRINT_DEC 1, al
-    ;NEWLINE
-    ;PRINT_DEC 1, num
-    
-    ;add a, b (a = a + b)
-    ;a는 레지스터 or 메모리
-    ;b는 레지스터 or 메모리 or 상수
-    ; 단! a, b 모두 메모리는 X
-    
-    ;빼기 연산 == 더하기 연산
-    ;sub a, b (a= a-b)
-    
-    add al, 1; 레지스터 + 상수
-    PRINT_DEC 1, al ; 1+1=2
-    NEWLINE
-    
-    add al, [num] ; 레지스터 + 메모리
-    ;그냥 num은 주소값 복사 -> 값 복사 대괄호 필수
-    PRINT_DEC 1, al ; 2+2=4
-    NEWLINE
-    
-    mov bl, 3 ; 레지스터 + 레지스터
-    add al, bl
-    PRINT_DEC 1, al ; 4+3=7
-    NEWLINE
-    
-    add [num], byte 1 ; 메모리 + 상수
-    PRINT_DEC 1, [num] ;1+2=3
-    NEWLINE
-    
-    add [num], al ; 메모리 + 레지스터
-    PRINT_DEC 1, [num] ;3+7=10
-    NEWLINE
-    
-    ; 곱하기 연산
-    ; mul reg
-    ; - mul bl => al * bl 
-    ; -- 연산 결과를 ax에 저장
-    ; - mul bx => ax* bx
-    ; -- 연산 결과는 dx(상위 16비트) ax(하위16비트)에 저장
-    
-    ; 예) 5*8 ?
-    mov ax, 0
-    mov al, 5
-    mov bl, 8
-    mul bl
-    PRINT_DEC 2,ax
-    NEWLINE
-    
-    ; 나누기 연산 (1바이트 나누기)
-    ; div teg
-    ; - div bl => ax / bl
-    ; -- 연산결과는 al(몫) ah(나머지)
-    
-    ;예) 100/3 ?
-    mov ax, 100
-    mov bl, 3
-    div bl
-    PRINT_DEC 1,al
-    NEWLINE
-    mov al, ah ;ah 값 al에 복사해서 출력
-    PRINT_DEC 1,al
-
-    xor rax, rax
+   ; 쉬프트(shit)연산
+   
+   mov eax, 0x12345678
+   PRINT_HEX 4, eax
+   NEWLINE
+   shl eax, 8 ;왼쪽으로 8칸 shift == 최상위 8bit는 분실이 된다는 의미
+   PRINT_HEX 4, eax
+   NEWLINE
+   shr eax, 8 ;오른쪽으로 8칸 shift == 최하위 8bit 분실
+   PRINT_HEX 4, eax
+   NEWLINE
+   ; 왜 ? 빠른 곱셈/나눗셈
+   ; 게임서버에서 각각 캐릭터의 ObjectID를 만들어줄 때
+   
+   ; 논리(logical) 연산 => NOT AND OR XOR
+   ; 조건 A , B
+   ; not A : 0 이면 1, 1 이면 0
+   ; A and B : 둘 다 참(1)이면 1출력, 하나라도 아니면 0출력
+   ; A or B : 둘 중 하나라도 1이면 1출력, 둘 다 아니면 0출력
+   ; A xor B : 둘 다 1이거나 0이면 0출력, 서로 다르면 1출력
+   
+   mov al, 0b10010101
+   mov bl, 0b01111100
+   
+   and al, bl ; al and bl = 0b0001 0100 = 0x14
+   PRINT_HEX 1, al
+   NEWLINE
+   
+   not al ;0b1110 1011 = 0xeb
+   PRINT_HEX 1, al
+   NEWLINE
+   
+   ;응용사례: bitflag = 각 0과 1에 의미 부여
+   
+   mov al, 0b10010101 ;0x95
+   mov bl, 0b01111100 ;0x7c
+   
+   NEWLINE
+   PRINT_HEX 1, al
+   NEWLINE
+   
+   xor al, bl ; 0b1110 1001 = 0xe9
+   PRINT_HEX 1, al
+   NEWLINE
+   
+   xor al, bl ; 0b1001 0101 = 0x95
+   PRINT_HEX 1, al
+   NEWLINE
+   ; 동일한 값으로 xor 두번하면 자기 자신으로 되돌아오는 특성
+   ; 암호학에서 유용 - 대칭키 암호 (value xor key)
+   
+   xor al, al ;자기끼리 연산하면, == mov al, 0
+   PRINT_HEX 1, al ; 무조건 0 출력
+   
+    xor rax, rax ; return 0 과 같은 의미
     ret
     
     ; 초기화 된 데이터
