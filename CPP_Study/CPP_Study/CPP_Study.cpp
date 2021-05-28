f9c3646dccee93d67a3eaaa3945bde6316eeacce
tldmschl81 <chse0810@gmail.com>
2021-05-28 13:24:42 +0900

    3-반복문

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,43 +1,83 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 유의사항(Tip)
-
+// 오늘의 주제 : 반복문
 /*
-1) 변수의 유효범위
-	+ 전역변수
-	int hp = 10;
-	
-	+ 스택 
-		- { } 중괄호의 범위가 생존 범위
-		- 같은 이름 두번 사용할 때 문제
-
-2) 연산 우선순위
-	- 실수를 줄이기 위해 소괄호를 잘 이용하자
-
-3) 타입 변환 (바구니 교체, 형식 교체) //캐스팅
-
-4) 사칙 연산 관련
-	
+	데이터를 메모리에 할당하고 가공하고의 방법에 대해 알아봄
+	가공한 데이터를 이용해서 무엇인가를 하고 싶다면?	
 */
 int main()
 {
-	int hp = 77777; // 4byte
+	// while
+	// if-else 굉장히 유용
+	// 하지만 한번만 실행하는게 아니라, 특정조건까지 계속 반복해야 하는 상황
+	// ex1) 게임을 끌 떄까지 계속 게임을 실행해라
+	// ex2) 목적지에 도달할 때까지 계속 이동하라
 
-	short hp2 = (short)hp; 
-	// 데이터 분실 주의
-	// 2byte = 고로 상위 비트에 저장되어있던 값들은 날라가고 남은 하위비트 값만 유지
+	int count = 0;
+
+	while (count < 5) {
+		cout << "Hello World" << endl;
+		count++;
+	}
+
+	do { // 무조건 한번은 괄호 안 내용 실행 후 조건식 판단
+		cout << "Hello World" << endl;
+		count++;
+	} while (count < 5);
+
+	// for
+	// - 정해진 횟수를 원할 때
 	
-	float hp3 = (float)hp;
-	// 실수로 변환할 때 정밀도 차이가 있기 떄문에 데이터 손실
+	for (int count2 = 0 ; count2 < 5 ; count2++) {
+		cout << "Hello World" << endl;
+	}
 
-	unsigned int hp4 = (unsigned int)hp;
-	// 비트 단위로 보면 똑같은데, 분석하는 방법이 달라져 다른 숫자로 출력(부호 주의)
+	// break; = 반복문에서 빠져나가주세요
+	// continue; = 아래있는 명령문을 skip하고 반복 재실행
 
-	// 곱셈
-	// - 오버플로우
-	// 나눗셈
-	// - 0 나누기 조심 => 크래쉬 남
-	// - 실수 관련; int / int = int로 인식 => 소수점 계산 불가
-	// - int / float = float (실수가 우선순위가 더 높음)
+	int round = 1; 
+	int hp = 100;
+	int damage = 10;
+
+
+	// 무한 루프 : 전투 시작
+	while (true) {
+		hp -= damage;
+		if (hp < 0)
+			hp = 0; //음 수체력을 0으로 보정
+
+		// 시스템 메시지
+		cout << "Round" << round << "몬스터체력" << hp << endl;
+
+		if (hp == 0) {
+			cout << "몬스터 처치!" << endl;
+			break;
+		}
+
+		if (round == 5) {
+			cout << "제한 라운드 종료" << endl;
+			break;
+		}
+
+		round++;
+	}
+
+	// 1~10 사이의 홀수만 출력하기
+
+	for (int i = 1; i <= 10; i++) {
+		/*
+		bool isEven = ((i % 2) == 0);
+			1)	if(isEven == false)
+					cout << i << endl;
+
+			2)	if(isEven)
+					continue; = 흐름 가로채서 다음 제어문 시작
+		*/
+
+		if(i % 2 == 1){
+			cout << i << endl;
+		}
+	}
 }	
+
