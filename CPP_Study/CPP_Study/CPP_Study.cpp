7cd11c747a83cfdb51701e1f371cbf1c71e2f204
tldmschl81 <chse0810@gmail.com>
2021-06-02 20:20:02 +0900

    4-호출스택

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,38 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 지역 변수와 '값' 전달 (call by value)
+// 오늘의 주제 : !!!호출 스택!!! = 내 지금 함수가 어떤 경로로 호출 되게 됐는지 경로 역탐색
+// 어셈블리언어에서 보면 함수를 호출 스택에서 가리키는게 아니라 그 다음 주소를 가리켜주고 있음(어디로 갈지에 대한)
+// 다만, c++언어에서는 부정확하게 나타내어질 수 있으니 그닥 크게 신경 X (호출 코드가 한 줄이 아니기에 착시현상이 있음)
 
-// 전역 변수 - 일부러 올려주지 않는 이상, 데이터 영역에 메모리 업로드
-// 참고) 초기화 여부, const 등 .rodata/ .data/ .bss
-int globalValue = 0;
+// 함수 선언 <-> 구현부
+void Func1();
+void Func2(int, int); // 아직 매개변수를 사용하지 않으니, 굳이 이름 부여를 선언에서 안해도 되긴함.
+void Func3(float a);
 
-void Test() {
-	cout << "전역 변수의 값은 : " << globalValue << endl; // 성공
+// 함수 구현
+void Func1() {
+	cout << "Func1" << endl;
+
+	// push2
+	// push 1
+	// call Func2
+	Func2(1,2);
 }
 
-void IncreaseHp(int hp) {
-	hp += 1;
+void Func2(int a, int b) {
+	cout << "Func2" << endl;
+
+	Func3(10);
 }
 
-// [매개변수][RET][지역변수] ---넘겨줄때 지역변수를 매개변수에 '복사'해서'값'만 넘겨주게됨--> [매개변수][RET][지역변수]
+void Func3(float a) {
+	cout << "Func3" << endl;
+
+}
+
 int main()
 {
-	int hp = 1;
-
-	cout << "Increase 호출 전 :" << hp << endl;
-	IncreaseHp(hp);
-	cout << "Increase 호출 후 :" << hp << endl;
-
-
-	cout << "전역 변수의 값은 : " << globalValue << endl; // 성공
-	globalValue++;
-
-	Test();
-
-	// 지역 변수 : 함수 내부에 만든 변수 - 스택 영역에 메모리 업로드, 함수끼리 변수 공유 불가
-	int localValue = 1;
-
+	cout << "main" << endl;
+	Func1();
 	return 0;
 }	
 
