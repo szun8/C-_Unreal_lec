4538eeb2ece6eab71d968b2aec72bfd2ae120298
tldmschl81 <chse0810@gmail.com>
2021-06-05 14:35:50 +0900

    4-TextRPG

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,48 +1,194 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 함수 마무리
+// 오늘의 주제 : TextRPG
 
-// 1. 오버로딩 (중복정의 : 함수 이름의 재사용)
-// - 매개변수 갯수 상이
-// - 매개변수 타입 상이 (순서가 다른걸 포함)
+enum PlayerType {
+	PT_Knight=1,
+	PT_Archer,	// 2
+	PT_Mage		// 3
+};
 
-int Add(int a, int b) {
-	return a + b;
-}
+enum MonsterType {
+	MT_Slime = 1,
+	MT_Orc,	// 2
+	MT_Skeleton	// 3
+};
 
-float Add(float a, float b) {
-	return a + b;
-}
-// 어디로 갈 지 애매하지 않으면 문제 X
+struct ObjectInfo { // 구조체
+	short type;
+	int hp;
+	char attack;
+	int defence;
+	// 이론상 2+4+1+4 = 11bytes
+	// 그러나 이 구조체의 크기는 11이 아닐수도 있음 
+	// ---> 메모리영역에 padding 값을 같이 넣어줘서 컴퓨터가 계산하기 쉽게 정렬함 (나중에 더 이야기해볼거임.)
+	// 실제로 까보면 short를 4byte로 잡고 있음
+};
 
-// 2. 기본인자값 (디폴트 매개변수)
-// 항상 마지막 끝 쪽에 와야함.
-void SetPlayerInfo(int hp, int mp, int attack, int guildId = 0, int castleId = 0) {
+ObjectInfo playerInfo;
+ObjectInfo monsterInfo;
 
-}
-
-// 3. 스택 오버플로우 : 스택 메모리 고갈
-
-int Factorial(int n) { // 재귀함수 호출
-	if (n <= 1)
-		return 1;
-
-	return n * Factorial(n - 1);
-}
+void EnterLobby();
+void SelectPlayer();
+void EnterField();
+void CreateRandomMonster();
+void EnterBattle();
 
 int main()
 {
-	float result = Add(1.5f, 2.1f);
-	cout << result << endl; // 3.6
+	// 랜덤 시드 설정
+	srand(time(0));
 
-	SetPlayerInfo(100, 40, 10);
-	// castleId를 설정하고자 할때는 guildId도 C++에서는 설정해줘야함 ---> C#은 castleId:1 이렇게 다로 설정가능
-
-	// 5! = 5*4*3*2*1
-	int result2 = Factorial(5000000);
-	cout << result2 << endl; // 3.6
+	EnterLobby();
 
 	return 0;
 }	
+
+void EnterLobby() {
+	while (1) {
+		cout << "--------------------" << endl;
+		cout << "로비에 입장했습니다!" << endl;
+		cout << "--------------------" << endl;
+
+		// 플레이어 직업 선택
+		SelectPlayer();
+
+		cout << "---------------------------" << endl;
+		cout << "(1) 필드 입장 (2) 게임 종료" << endl;
+		cout << "---------------------------" << endl;
+
+		int input;
+		cin >> input;
+
+		if (input == 1) {
+			EnterField();
+		}
+		else {
+			return;
+		}
+	}
+}
+
+void SelectPlayer() {
+	while (1) {
+		cout << "------------------" << endl;
+		cout << "직업을 골라주세요!" << endl;
+		cout << "(1) 기사 (2) 궁수 (3) 법사" << endl;
+		cout << "> ";
+
+		cin >> playerInfo.type; //input
+		if (playerInfo.type == PT_Knight) {
+			cout << "기사 생성중..." << endl;
+			playerInfo.hp = 150;
+			playerInfo.attack = 10;
+			playerInfo.defence = 5;
+			break;
+		}
+		else if (playerInfo.type == PT_Archer) {
+			cout << "궁수 생성중..." << endl;
+			playerInfo.hp = 100;
+			playerInfo.attack = 15;
+			playerInfo.defence = 3;
+			break;
+
+		}
+		else if (playerInfo.type == PT_Mage) {
+			cout << "법사 생성중..." << endl;
+			playerInfo.hp = 80;
+			playerInfo.attack = 25;
+			playerInfo.defence = 0;
+			break;
+
+		}
+		// 세 개도 아니라면 다시 반복문 돌아서 직업선택!
+	}
+}
+
+void EnterField() {
+	while (1) {
+		cout << "--------------------" << endl;
+		cout << "필드에 입장했습니다!" << endl;
+		cout << "--------------------" << endl;
+
+		cout << "[PLAYER] HP : " << playerInfo.hp << " / ATT : " << playerInfo.attack << " / DEF : " << playerInfo.defence << endl;
+		CreateRandomMonster();
+
+		cout << "--------------------" << endl;
+		cout << "(1) 전투 (2) 도주" << endl;
+		cout << "> ";
+
+		int input;
+		cin >> input;
+
+		if (input == 1) {
+			EnterBattle();
+			if (playerInfo.hp == 0)
+				return;
+		}
+		else
+			return;
+	}
+}
+
+void CreateRandomMonster() {
+	// 1~3 랜덤 출력
+	monsterInfo.type = 1 + (rand() % 3);
+
+	switch (monsterInfo.type)
+	{
+	case MT_Slime:
+		cout << " 슬라임 생성중...! (HP:15 / ATT:5 / DEF:0)" << endl;
+		monsterInfo.hp = 15;
+		monsterInfo.attack = 5;
+		monsterInfo.defence = 0;
+		break;
+	case MT_Orc:
+		cout << " 오크 생성중...! (HP:40 / ATT:10 / DEF:3)" << endl;
+		monsterInfo.hp = 40;
+		monsterInfo.attack = 10;
+		monsterInfo.defence = 3;
+		break;
+	case MT_Skeleton:
+		cout << " 스켈레톤 생성중...! (HP:80 / ATT:15 / DEF:5)" << endl;
+		monsterInfo.hp = 80;
+		monsterInfo.attack = 15;
+		monsterInfo.defence = 5;
+		break;
+	}
+}
+
+void EnterBattle() {
+	while (1) {
+		int damage = playerInfo.attack - monsterInfo.defence;
+		if (damage < 0)
+			damage = 0;
+
+		//선빵
+		monsterInfo.hp -= damage;
+		if (monsterInfo.hp < 0)
+			monsterInfo.hp = 0;
+
+		cout << "몬스터 남은 체력 : " << monsterInfo.hp << endl;
+		if (monsterInfo.hp == 0) {
+			cout << "몬스터를 처치했습니다!" << endl;
+			return; // break;
+		}
+		
+		damage = monsterInfo.attack - playerInfo.defence;
+		if (damage < 0)
+			damage = 0;
+
+		// 반격
+		playerInfo.hp -= damage;
+		if (playerInfo.hp < 0)
+			playerInfo.hp = 0;
+
+		cout << "플레이어 남은 체력 : " << playerInfo.hp << endl;
+		if (playerInfo.hp == 0) {
+			cout << "당신은 사망했습니다... GAME OVER" << endl;
+			return; // break;
+		}
+	}
+}
 
