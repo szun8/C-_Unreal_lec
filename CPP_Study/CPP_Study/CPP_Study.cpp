b59e4bee0ecad35b6b7d0d517ef6cea05d0681c3
tldmschl81 <chse0810@gmail.com>
2021-06-05 13:15:49 +0900

    4-함수마무리

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,40 +1,48 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : !!!호출 스택!!! = 내 지금 함수가 어떤 경로로 호출 되게 됐는지 경로 역탐색
-// 어셈블리언어에서 보면 함수를 호출 스택에서 가리키는게 아니라 그 다음 주소를 가리켜주고 있음(어디로 갈지에 대한)
-// 다만, c++언어에서는 부정확하게 나타내어질 수 있으니 그닥 크게 신경 X (호출 코드가 한 줄이 아니기에 착시현상이 있음)
+// 오늘의 주제 : 함수 마무리
 
-// 함수 선언 <-> 구현부
-void Func1();
-void Func2(int, int); // 아직 매개변수를 사용하지 않으니, 굳이 이름 부여를 선언에서 안해도 되긴함.
-void Func3(float a);
+// 1. 오버로딩 (중복정의 : 함수 이름의 재사용)
+// - 매개변수 갯수 상이
+// - 매개변수 타입 상이 (순서가 다른걸 포함)
 
-// 함수 구현
-void Func1() {
-	cout << "Func1" << endl;
-
-	// push2
-	// push 1
-	// call Func2
-	Func2(1,2);
+int Add(int a, int b) {
+	return a + b;
 }
 
-void Func2(int a, int b) {
-	cout << "Func2" << endl;
+float Add(float a, float b) {
+	return a + b;
+}
+// 어디로 갈 지 애매하지 않으면 문제 X
 
-	Func3(10);
+// 2. 기본인자값 (디폴트 매개변수)
+// 항상 마지막 끝 쪽에 와야함.
+void SetPlayerInfo(int hp, int mp, int attack, int guildId = 0, int castleId = 0) {
+
 }
 
-void Func3(float a) {
-	cout << "Func3" << endl;
+// 3. 스택 오버플로우 : 스택 메모리 고갈
 
+int Factorial(int n) { // 재귀함수 호출
+	if (n <= 1)
+		return 1;
+
+	return n * Factorial(n - 1);
 }
 
 int main()
 {
-	cout << "main" << endl;
-	Func1();
+	float result = Add(1.5f, 2.1f);
+	cout << result << endl; // 3.6
+
+	SetPlayerInfo(100, 40, 10);
+	// castleId를 설정하고자 할때는 guildId도 C++에서는 설정해줘야함 ---> C#은 castleId:1 이렇게 다로 설정가능
+
+	// 5! = 5*4*3*2*1
+	int result2 = Factorial(5000000);
+	cout << result2 << endl; // 3.6
+
 	return 0;
 }	
 
