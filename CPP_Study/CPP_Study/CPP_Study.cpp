83b4e266ddca8752ffdd1b5f15e71649a67e2129
tldmschl81 <chse0810@gmail.com>
2021-06-12 13:55:03 +0900

    5-포인터vs참조

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,15 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 참조
-
-// 원본 데이터를 수정하고자 하는데, 
-// (주소 전달 방식인) 포인터로 매개변수를 넘겨주지 않는다면,
-// (값 전달방식을 이용한다면,)
-// 무조건 값복사라고 생각하고 원본 값은 조정되지 않는다
-
-// 값을 읽기만을 원한다면, (예를들어 print)
-// 값 전달 방식도 문제는 없
+// 오늘의 주제 : 포인터 vs 참조
 
 struct StatInfo {
 	int hp;
@@ -23,23 +15,34 @@ void CreateMonster(StatInfo* info) {
 	info->defence = 5;
 }
 
-// 1) 값 전달 방식
-void PrintInfoByCopy(StatInfo info) {
-	cout << "---------------------" << endl;
-	cout << "HP : " << info.hp << endl;
-	cout << "ATT : " << info.attack << endl;
-	cout << "DEF : " << info.defence << endl;
-	cout << "---------------------" << endl;
-}
+StatInfo globalInfo;	
 
 // 2) 주소 전달 방식
-void PrintInfoByPtr(StatInfo* info) {
+void PrintInfoByPtr(const StatInfo* const info) {
+	// 포인터가 가지는 특성 상 만약의 경우를 대비해,
+	if (info == nullptr)
+		return;	// 아무값도 가지지 않는다면 종료해주는 조건문
+
 	cout << "---------------------" << endl;
 	cout << "HP : " << info->hp << endl;
 	cout << "ATT : " << info->attack << endl;
 	cout << "DEF : " << info->defence << endl;
 	cout << "---------------------" << endl;
-}
+
+	// 별 뒤에 붙인다면?
+	// StatInfo* const info
+	// info라는 바구니의 내용물인 주소를 바꿀 수 
+			// info = &globalInfo; (X)
+	// info는 주소값을 갖는 바구니 -> 이 주소값이 고정이다!
+
+	// 별 이전에 붙인다면?
+	// const StatInfo* info
+	// info가 가리키고 있는 바구니의 내용물을 바꿀 수 없음
+	// '원격' 바구니의 내용물을 바꿀 수 없음
+			// info->hp = 10000;  (X)
+
+	// info [ 주소값 ]	--->   주소값 [ 데이터 ]
+	}
 
 // StatInfo 구조체가 1000바이트짜리 큰 대형 구조체라면?
 // - (값 전달) StatInfo로 넘기면 1000바이트가 복사되는
@@ -51,7 +54,8 @@ void PrintInfoByPtr(StatInfo* info) {
 // 주소 전달처럼 주소 값을 이용해 진퉁을 건드리는!
 // 일석이조의 방식!
 
-void PrintInfoByRef(StatInfo& info) {
+void PrintInfoByRef(const StatInfo& info) { // 읽기 전용
+	// const 넣으면 값 변경 불가!
 	cout << "---------------------" << endl;
 	cout << "HP : " << info.hp << endl;
 	cout << "ATT : " << info.attack << endl;
@@ -59,40 +63,56 @@ void PrintInfoByRef(StatInfo& info) {
 	cout << "---------------------" << endl;
 }
 
+#define OUT	// 전처리를 사용해서 
+// 바뀐다는 것을 명시적으로 나타내주는 기능 사용
+void ChangeInfo(OUT StatInfo& info) {
+	info.hp = 1000;
+}
+
 int main()
 {
-	// 4바이트 정수형 바구니를 사용
-	// 앞으로 그 바구니 이름은 number
-	// 그러니까 number에서 뭘 꺼내거나, 뭘 넣는다고 하면
-	// 찰떡같이 알아듣고 해당 주소에 넣어주면 된다
-	int number = 1;
-
-	// * 
-	// 주소를 담는 바구니
-	// int 그 바구니를 따라가면 int 데이터(바구니)가 있음
-	int* pointer = &number;
-	// pointer 바구니에 있는 주소를 타고 이동해서, 그 멀리 있는 바구니에 2를 넣는다
-	*pointer = 2;
-
-	// 로우레벨(어셈블리) 관점에서 실제 작동 방식은 int*와 똑같음
-	// 실제로 실행해보면 포인터랑 100% 똑같다
-	int& reference = number;
-
-	// C++관점에서는 number라는 바구니에 또 다른 이름을 부여한 것.
-	// number라는 바구니에 reference라는 다른 이름을 지어줄게
-	// 앞으로 reference 바구니에다가 뭘 꺼내거나 넣으면,
-	// 실제 number 바구니(진퉁에다가) 그 값을 꺼내거나 넣으면 됨!
-	reference = 3;
-
-	// 같다고 한다면 왜 굳이 참조를 사용하는가?
-	// 참조 전달 때문!
-
 	StatInfo info;
 	CreateMonster(&info);
 
-	PrintInfoByCopy(info);	// 값 전달
-	PrintInfoByPtr(&info);	// 주소 전달
-	PrintInfoByRef(info);	// 참조 전달
+	// 성능 : 똑같음
+	// 편의성 : 참조 승! 
+
+	// 1) 편의성 관련
+	// 편의성이 좋다는게 꼭 장점만은 아니다
+	// 포인터는 주소를 넘기니 확실하게 원본을 넘긴다는 힌트를 줄수있는데.
+	// 참조는 자연스럽게 모르고 지나칠 수 있음
+	// 예) 마음대로 고친다면?
+	// const를 사용해 맘대로 사용하는 부분 개선 가능 (안정성 도모)
+
+	// 참고로 포인터도 const를 사용가능.
+	// 단, * 기준으로 앞에 붙이느냐, 뒤에 붙이느냐에 따라 의미가 달라짐
+
+	// 2) 초기화 여부
+	// 침조 타입은 바구니의 2번째 이름 (별칭?)
+	// -> 참조하는 대상이 없으면 안됨
+	// 반면 포인터는 그냥 어떤~ 주소라는 의미
+	// -> 대상이 실존하지 않을 수도 있음.
+
+	// 포인터에서 '없다'는 의미로?
+	StatInfo* pointer = nullptr; // 0 or NULL
+	// pointer = &info;
+	PrintInfoByPtr(pointer);	// 주소 전달
+
+	// 참조 타입은 이런 nullptr의 개념이 없다
+	StatInfo& reference = info;
+	PrintInfoByRef(reference);	// 참조 전달
+	ChangeInfo(OUT info);	// 그냥 info만 있을 때는 바뀐다는 힌트가 없는데,
+	// OUT을 같이 사용해서 바뀐다는 힌트를 부여해줌 (가독성 측면)
+
+	// BONUS 1) 포인터로 사용하던걸 참조로 넘겨주려면?
+	// pointer[  주소(&info)  ]		ref, info[  데이터  ]
+	StatInfo& ref = *pointer; // 새 별칭 지칭해주면 됨
+	PrintInfoByRef(ref); // *를 붙여주면 pointer가 가리키는 주소로 넘어가라!
+
+	// BONUS 2) 참조로 사용하던걸 포인터로 넘겨주려면?
+	// pointer[  주소(&info)  ]		reference, info[  데이터  ]
+	StatInfo* ptr = &reference;
+	PrintInfoByPtr(ptr);
 
 	return 0;
 }
