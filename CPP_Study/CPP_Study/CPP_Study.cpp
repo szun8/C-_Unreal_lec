4933638a4936bb3799ef70c5374fc912b1b9256f
tldmschl81 <chse0810@gmail.com>
2021-08-04 21:18:28 +0900

    10-함수포인터2

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,87 +3,68 @@ using namespace std;
 
 // 오늘의 주제 : 함수 포인터
 
-int Add(int a, int b) {
+// typedef ? : 메모리에 올라가는 개념이 아님, 그냥 있다정도로 선언하기에 불과
+// typedef 왼쪽 오른값 -> 오른쪽 (커스텀 타입 정의)
+
+// 사실 타입정의값이 복잡해질수록 왼/오 로만 구분하기 어려워짐
+// 정확히는 왼/오 기준이 아니라,
+// [선언 문법]에서 typedef을 앞에다 붙이는 쪽
+
+typedef int(*PFUNC)(int, int);	//어디에 포인터를 붙이는지 괄호로 정확하게 표기하기!
+int Test(int a, int b) {
+
+	cout << "Test" << endl;
 	return a + b;
 }
 
-int Sub(int a, int b) {
-	return a - b;
-}
-
-class Item {
+class Knight {
 public:
-	Item() : _itemId(0), _rarity(0), _ownerId(0) {
+	// 멤버함수
+	int GetHp(int a , int b) {
+		return _hp;
 	}
-
 public:
-	int _itemId;	// 아이템 구분 ID
-	int _rarity;	// 희귀도
-	int _ownerId;	// 소지자 ID
+	int _hp = 100;
 };
 
-// 예를들어, 인벤토리에서 원하는 아이템을 찾고자할 때 기준에 따른 함수를 여러개 만들어버리면 굉장히 유지보수성 하락
-// typedef 함수를 사용하면, 하나의 함수에 여러 기준 대입 가능 (각 기준마다 조건은 달라지는것만 생성해주면!)
-
-typedef bool(ITEM_SELECTOR)(Item*, int);
-
-Item* FindItem(Item items[],int itemCount, ITEM_SELECTOR* selector, int value) {	// = bool(*selector)(Item* item);
-	for (int i = 0; i < itemCount; i++) {
-		Item* item = &items[i];
-		if (selector(item, value))
-			return item;
-	}
-	return nullptr;
-}
-
-#pragma region SELECTOR
-bool IsRareItem(Item* item, int value) {
-	return item->_rarity >= value;
-}
-
-bool IsOwnerItem(Item* item, int ownerId) {
-	return item->_rarity == ownerId;
-}
-#pragma endregion
+// 그렇다면 class의 멤버함수 포인터는 ?
+typedef int(Knight::*PMEMFUNC)(int, int);
+// Knight(Class)안에 포함된 멤버함수 포인터를 정의해줘
 
 int main()
 {
-	// pointer
-	int a = 10;
-	typedef int DATA;	// int -> DATA
-	// 1) 포인터		*
-	// 2) 변수 이름		ptr
-	// 3) 데이터 타입	int(DATA)
-	DATA* ptr = &a;
-
 	// function
 	// 함수(반환형-int, 매개변수 int 2개를 받는)를 타입정의해서 사용하고 싶다면?
-	typedef int(FUNC_TYPE)(int , int ); // 형태
+	// 형태 : typedef int(FUNC_TYPE)(int , int ); 
 
 	// 같은 의미로 modern c++에서 아래와 같이 정의가능
-	//using FUNC_TYPE = int(int a, int b);
+	// using FUNC_TYPE = int(int a, int b);
 
 	// 1) 포인터		*
 	// 2) 변수 이름		fn
 	// 3) 데이터 타입	함수 (인자는 int, int/ 반환은 int)
-	FUNC_TYPE* fn;
+	// => int (*fn)(int, int);
 
-	// 함수포인터
-	fn = Add;
-	// 이걸 왜 쓰냐 ?
-	// 형태가 유사한 다른 함수를 적용하고 싶을때, (여러 함수중 하나를 골라서 호출하고 싶을때)
-	// 하나하나 이름 바꿀필요 없이 fn이 가리키는 함수 주소만 바꾸면 더 간단히 사용할 수 있음 
-	fn = Sub;
-
-	// 함수의 이름은 함수의 시작 주소 (배열과 유사)(!!!)
-	int result = fn(1, 2);	// = Add(1,2);, 기본문법
-	cout << result << endl;
+	PFUNC fn;
+	fn = &Test;		// & 생략 가능 (C언어 호환성 때문)
 	
-	int result2 = (*fn)(1, 2);	// 함수포인터는 *(접근 연산자) 붙어도 함수 주소!, 기본과 같은 의미
-	cout << result2 << endl;
+	fn(1, 2);
+	(*fn)(1, 2);
+	// 위 문법으로 [전역 함수 / 정적(static) 함수]만 담을 수 있다 (호출 규약이 동일한 애들)
+	// -> class 멤버 함수는 해당 문법 통과 X = 객체의 함수를 호출하는 것이기 때문에 그냥 함수를 호출하는 것과 태생이 다름
+	
+	Knight k1;
+	k1.GetHp(1, 1);
 
-	Item items[10] = {};
-	items[3]._rarity = 2;	// RARE
-	Item* rareItem = FindItem(items, 10, IsRareItem, 2);	// 함수 그 자체는 주소로 반응한다고 위에 적었다
+	// 멤버함수포인터 사용법
+	PMEMFUNC mfn;
+	mfn = &Knight::GetHp;	// 얘는 &(주소)표시 붙여줘야 통과
+	// 객체를 불러와야하기에 인스턴스를 같이 부여줘야하는 규약이 있음)
+	(k1.*mfn)(3,4);
+
+	Knight* k2 = new Knight();	// 동적할당
+	(k2->*mfn)(3, 4);	// * . = -> / ((*k2).*mfn)(3, 5);와 같은 의미
+
+	delete k2;
 	return 0;
 }
