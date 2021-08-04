2c379a6d1e9d1b03c644d746b72719ec4bb0dcdd
tldmschl81 <chse0810@gmail.com>
2021-08-04 21:40:54 +0900

    10-함수객체(Functor)

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,70 +1,65 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 함수 포인터
+// 오늘의 주제 : 함수 객체(Functor) = [함수처럼 동작]하는 객체
+// !!!!! () 연산자 오버로딩
 
-// typedef ? : 메모리에 올라가는 개념이 아님, 그냥 있다정도로 선언하기에 불과
-// typedef 왼쪽 오른값 -> 오른쪽 (커스텀 타입 정의)
+// 함수포인터의 단점
+// 1) 완전히 동일한 시그니처(반환, 매개변수 타입, 수 등)만 호환이 가능했음
+// 2) 상태를 가질 수 없다.
 
-// 사실 타입정의값이 복잡해질수록 왼/오 로만 구분하기 어려워짐
-// 정확히는 왼/오 기준이 아니라,
-// [선언 문법]에서 typedef을 앞에다 붙이는 쪽
-
-typedef int(*PFUNC)(int, int);	//어디에 포인터를 붙이는지 괄호로 정확하게 표기하기!
-int Test(int a, int b) {
-
-	cout << "Test" << endl;
-	return a + b;
-}
-
-class Knight {
+class Functor {
 public:
-	// 멤버함수
-	int GetHp(int a , int b) {
-		return _hp;
+	void operator() () {
+		cout << "Functor Test" << endl;
+		cout << _value << endl;
 	}
-public:
-	int _hp = 100;
-};
 
-// 그렇다면 class의 멤버함수 포인터는 ?
-typedef int(Knight::*PMEMFUNC)(int, int);
-// Knight(Class)안에 포함된 멤버함수 포인터를 정의해줘
+	bool operator() (int num) {
+		cout << "Functor Test" << endl;
+		_value += num;
+		cout << _value << endl;
+		return true;
+	}
+
+private:
+	int _value = 0;
+};
+class MoveTask {
+public:
+	void operator() () {
+		// TODO
+		cout << "해당 좌표로 플레이어 이동" << endl;
+	}
+
+public:
+	int _playerId;
+	int _posX;
+	int _posY;
+};
 
 int main()
 {
-	// function
-	// 함수(반환형-int, 매개변수 int 2개를 받는)를 타입정의해서 사용하고 싶다면?
-	// 형태 : typedef int(FUNC_TYPE)(int , int ); 
+	Functor functor;
 
-	// 같은 의미로 modern c++에서 아래와 같이 정의가능
-	// using FUNC_TYPE = int(int a, int b);
+	functor();	// ()연산자 오버로딩으로 가능해짐
+	bool ret = functor(3);
 
-	// 1) 포인터		*
-	// 2) 변수 이름		fn
-	// 3) 데이터 타입	함수 (인자는 int, int/ 반환은 int)
-	// => int (*fn)(int, int);
+	// MMO에서 함수객체를 사용하는 예시
+	// 클라이언트 <-> 서버
+	// 서버 : 클라가 보내준 네트워크 패킷을 받아서 처리
+	// ex) 클라 : 나 (5,0) 좌표로 이동시켜줘!
+	// 만약에 이런 요청들이 많이 몰린다면?
 
-	PFUNC fn;
-	fn = &Test;		// & 생략 가능 (C언어 호환성 때문)
-	
-	fn(1, 2);
-	(*fn)(1, 2);
-	// 위 문법으로 [전역 함수 / 정적(static) 함수]만 담을 수 있다 (호출 규약이 동일한 애들)
-	// -> class 멤버 함수는 해당 문법 통과 X = 객체의 함수를 호출하는 것이기 때문에 그냥 함수를 호출하는 것과 태생이 다름
-	
-	Knight k1;
-	k1.GetHp(1, 1);
+	MoveTask task;
+	// 일단 요청 주문서를 만들어놓긴 함
+	task._playerId = 100;
+	task._posX = 5;
+	task._posY = 0;
 
-	// 멤버함수포인터 사용법
-	PMEMFUNC mfn;
-	mfn = &Knight::GetHp;	// 얘는 &(주소)표시 붙여줘야 통과
-	// 객체를 불러와야하기에 인스턴스를 같이 부여줘야하는 규약이 있음)
-	(k1.*mfn)(3,4);
-
-	Knight* k2 = new Knight();	// 동적할당
-	(k2->*mfn)(3, 4);	// * . = -> / ((*k2).*mfn)(3, 5);와 같은 의미
-
-	delete k2;
+	// 나중에 여유가 될때 일감을 실행한다
+	task();
+	// => functor를 만들어주는 시점과 실행하는 시점을 분리할 수 있다는 굉장히 큰 장점 소유
+	// commandPattern 이라는 용어로 부르게 될거랍니다,,,
 	return 0;
 }
