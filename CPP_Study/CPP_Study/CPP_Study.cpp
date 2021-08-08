cd41032ddff1612122ad68ca150fe3ee4624e6d9
tldmschl81 <chse0810@gmail.com>
2021-08-08 19:45:39 +0900

    10-콜백

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,133 +1,65 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 템플릿 기초
+// 오늘의 주제 : 콜백 callback
+// : 다시 호출하다? 역으로 호출하다?
+// 함수 포인터 + 함수 객체 + 템플릿
 
-// 템플릿 : 함수나 클래스를 찍어내는 틀
-// 1) 함수 템플릿
-// 2) 클래스 템플릿
-//-----------------------------------------------------
+// 어떤 상황이 일어나면 -> 이 기능 호출해줘
 
-#pragma region 함수 템플릿
-template<typename T>
-// template<class T> 도 같은 의미로 가능하다!
-// type별로 아래 함수를 재사용하는 것이 아니라,
-// type별로 함수를 새로 만들어 주는 것
-void Print(T a) {
-	cout << a << endl;
-}
-
-// 아래와 같이 type을 두개 받아서
-// 꼭 굳이 동일한 type의 매개변수를 받지 않아도 됨을 설명
-template<typename T1, typename T2>
-void Print(T1 a, T2 b) {
-	cout << a << " " << b << endl;
-}
-
-template<typename T>
-T Add(T a, T b) {
-	return a + b;
-}
-
-class Knight {
+class Item {
 public:
 
 public:
-	int _hp = 100;
+	int _itemId = 0;
+	int _rarity = 0;
+	int _ownerId = 0;
 };
 
-// 우리가 특수하게 만든 class의 내용도 출력하고자 할 때,
-// 
-// 연산자 오버로딩 (전역함수.ver)
-// cout -> ctrl + f12 = 어디서 정의됐는지 알수 있음
-ostream& operator<< (ostream& os, const Knight& k) {
-	os << k._hp;
-	return os;
-}
-
-// 템플릿 특수화
-template<>
-void Print(Knight a) {
-	cout << "!Knight!" << endl;
-	cout << a._hp << endl;
-}
-#pragma endregion
-
-#pragma region 클래스 템플릿
-
-// typename '?'를 붙이면 '조커카드' (어떤 타입도 나 넣을 수 있음)
-// 근데 무조건 typename을 붙여야 하는 것은 아님
-template<typename Type, int SIZE=10>
-// 디폴트 매개변수도 가능!
-class RandomBox {
+class FindByOwnerId {
 public:
-	Type GetRandomData() {
-		int idx = rand() % SIZE;
-		return _data[idx];
+	bool operator()(const Item* item) { // compare => const
+		return (item->_ownerId == _ownerId);
 	}
 public:
-	Type _data[SIZE];
+	int _ownerId;
 };
 
-// 템플릿 특수화
-template<int SIZE>
-// 근데 특수화에선 디폴트 매개변수가 안된댕
-// +)
-// 특수화 시에는 기본 클래스 템플릿과 다르다는 것을 구분해주기 우해서
-// 원하는 타입을 아래처럼 클래스 이름 옆 대괄호에 정의해준다
-class RandomBox<double, SIZE> {
+class FindByRarity {
 public:
-	double GetRandomData() {
-		cout << "RandomBox Double" << endl;
-		int idx = rand() % SIZE;
-		return _data[idx];
+	bool operator()(const Item* item) { // compare => const
+		return (item->_rarity >= _rarity);
 	}
 public:
-	double _data[SIZE];
+	int _rarity;
 };
-#pragma endregion
 
+// 마지막 매개변수로, 함수 포인터 = bool(*func)(const Item*) 을 넣거나
+// 아래처럼 템플릿-함수객체(연산자 오버로딩)을 이용해 자유도 높은 기준 조건 생성가능 
+template<typename T>
+// T selector에 뭐를 넣을지에따라 functor가 알아서 맞는 기준 조건 호출!
+Item* FindItem(Item items[], int itemCount, T selector) {
+	for (int i = 0; i < itemCount; i++) {
+		Item* item = &items[i];
 
+		if(selector(item))	// class 마다 functor의 특징은 공통적으로 가짐
+			return item;
+	}
+	return nullptr;
+}
 int main()
 {
-	srand(static_cast<unsigned int>(time(nullptr)));
+	Item items[10];
+	items[3]._ownerId = 100;
+	items[8]._rarity = 2;
 
-	// 알아서 type을 컴파일러가 추론해서 템플릿에 넣어주는 것
-	Print<int>(50);
-	// 대신 대괄호로 적어서 명시적으로 우리가 정해줄수도 있다.
-	Print(50.5f);
-	Print(50.0);
-	Print("Hello World");
+	FindByOwnerId functor1;
+	functor1._ownerId = 100;
 	
-	Print("Hello World",100);
+	FindByRarity functor2;
+	functor2._rarity = 1;
 
-	Add(1, 2);
-	Add<float>(1.11f, 2.22f);
-
-	Knight k1;
-	Print(k1);
-
-	// --------------------------------------
-
-	RandomBox<int,10> rb1;
-
-	for (int i = 0; i < 10; i++) {
-		rb1._data[i] = i;
-	}
-	int value1 = rb1.GetRandomData();
-	cout << value1 << endl;
-
-	RandomBox<double, 20> rb2;
-
-	for (int i = 0; i < 20; i++) {
-		rb2._data[i] = i + 0.5;
-	}
-	int value2 = rb2.GetRandomData();
-	cout << value2 << endl;
-
-	// rb1 = rb2; => 결론적으로 같지 않게 인식
-	// 비슷한 유형의 랜덤박스로 보일지라도 템플릿 인자로 받을때
-	// 서로 다른 수가 있다면 개와 고양이 처럼
-	// 완전히 다른 클래스 유형으로 인식함
+	Item* item1 = FindItem(items, 10, functor1); // -> FindByOwnerId
+	Item* item2 = FindItem(items, 10, functor2); // -> FindByRarity
 	return 0;
 }
