03422092dd922e17fafc6e0923a5e6c0090a3d61
tldmschl81 <chse0810@gmail.com>
2021-08-15 17:41:33 +0900

    11-vector #3

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,89 +3,148 @@
 using namespace std;
 
 // 오늘의 주제 : vector
+// 우리가 vector 기능을 구현해보는 게 오늘 내용
 
-// Container : 데이터를 저장하는 객체 (자료구조 Data Structure)
+template<typename T>
+class Iterator {
+public:
+	Iterator() : _ptr(nullptr) {
 
-// - vector의 동작 원리 (size/capacity)
+	}
+	
+	Iterator(T* _ptr) : _ptr(_ptr) {
 
-// 반복자 iterator : 포인터와 유사한 개념, 컨테이너의 원소(데이터)를 가리키고, 다음/이전 원소로 이동 가능
+	}
+
+	Iterator& operator++() {	// 전위
+		_ptr++;
+		return *this;
+	}
+	 
+	Iterator operator++(int) {  // 후위
+		Iterator temp = *this;
+		_ptr++;
+		return temp;
+	}
+	
+	Iterator& operator--() {	// 전위
+		_ptr--;
+		return *this;
+	}
+	 
+	Iterator operator--(int) {  // 후위
+		Iterator temp = *this;
+		_ptr--;
+		return temp;
+	}
+	
+	Iterator operator+(const int count) { 
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+	Iterator operator-(const int count) {
+		Iterator temp = *this;
+		temp._ptr -= count;
+		return temp;
+	}
+
+	bool operator==(const Iterator& right) {
+		return _ptr == right._ptr;
+	}
+	
+	bool operator!=(const Iterator& right) {
+		return _ptr != right._ptr;
+		// return !(*this == right);
+	}
+
+	T& operator*() {
+		return *_ptr;
+	}
+
+public:
+	T* _ptr;
+};
+template<typename T>
+class Vector {
+public:
+	Vector() : _data(nullptr), _size(0), _capacity(0) {}
+	~Vector() {
+		if (_data)
+			delete[] _data;
+	}
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+	T& operator[] (const int pos) { return _data[pos]; }
+	// & ref안해주면 참조는 되는데 값 변경은 안되기에 붙여주기~
+	void clear() { _size = 0; }
+	void push_back(const T& val) {
+		if (_size == _capacity) {
+			// 메모리 증설 작업
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+			if (newCapacity == _capacity)	newCapacity++;
+			reserve(newCapacity);
+		}
+
+		// 이제부터 여유공간이있음!
+
+		// 데이터 저장
+		_data[_size] = val;
+		// 데이터 개수 증가
+		_size++;
+	}
+
+	void reserve(int capacity) {
+		_capacity = capacity;
+		T* newData = new T[_capacity];
+
+		// 데이터 복사
+		for (int i = 0; i < _size; i++) {
+			newData[i] = _data[i];
+		}
+
+		// 새로 복사했으니, 기존에 있던 데이터를 날린다.
+		if (_data) delete[] _data;
+		// 교체
+		_data = newData;
+	}
+
+public:
+	typedef Iterator<T> iterator;
+
+	iterator begin() { return iterator(&_data[0]); }
+	iterator end() { return begin() + _size; }
+
+private:
+	T* _data;
+	int _size;
+	int _capacity;
+};
 
 int main()
 {
-	vector<int> v(10);
-	v.reserve(1000); //중간 메모리 이사를 막기 위해 capacity 지정
-	// int 대신!
-	for (vector<int>::size_type i = 0; i < v.size(); i++) {
-		v[i] = i;
+	vector<int> v;
+	v.reserve(100);
+
+	for (int i = 0; i < 100; i++) {
+		v.push_back(i);
+		cout << v.size() << " " << v.capacity() << endl;
+	}
+	
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
 	}
 
-	/*vector<int>::iterator it;
-	it = v.begin();
-	cout << (*it) << endl;
+	cout << "----------------------------" << endl;
 
-	int* ptr;
-	ptr = &v[0];
-	cout << (*ptr) << endl;*/
-
-	vector<int>::iterator itBegin = v.begin();
-	vector<int>::iterator itEnd = v.end();
-	// 유효한 범위 다음의 범위를 가리키는 end()! => 즉 원치않은 쓰레기 값을 가지고 있다
-
-	// (!) 아주 미세한 성능 차 = ++it > it++(복사하는 과정이 있기 때문에)
-
-	// 위에 보다 더 복잡해보이는데? 
-	// 다른 컨테이너는 v[i]와 같은 index 접근이 안될수도 있음
-	// iterator는 vector뿐 아니라, 다른 컨테이너에도 공동적으로 있는 개념
-	// 그래서 뭐 list<int>::iterator it 처럼 보다 쉬운 코드호환이 가능함
 	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-		cout << *it << endl;
+		cout << (*it) << endl;
 	}
 
-	int* ptrBegin = &v[0]; // same mean = v.begin()._Ptr;
-	int* ptrEnd = ptrBegin + 10; // same mean = v.end()._Ptr;
-	for (int* ptr = ptrBegin; ptr != ptrEnd; ++ptr) {
-		cout << *ptr << endl;
-	}
+	v.clear();
 
-	// const int*;  only read , 수정 X
-	vector<int>::const_iterator cit1 = v.cbegin();
-
-	// 역방향 이동 사용자도 있음 로꾸거~
-	for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
-		cout << *it << endl;
-	}
-
-	// vector = 동적 배열 = 동적으로 커지는 배열 = 배열
-	// 원소가 하나의 메모리 블록에 연속하게 저장된다 !!! (중간에 빈 값이 있는것 용납 X)
-
-	// 기초 지식이기에 면접 단골 문제라고 합니다
-	// 	   그래서 이 부분을 모른다면 실력이 ㅎㅎ 여기까지~ 아는게 좋겠죠?
-	// - 중간(BAD) 삽입/삭제 => 삽입/삭제한만큼 기존에 있던 데이터들의 위치를 수정해줘야하기 때문에 엄청난 나비효과를 불러옴
-	// - 처음(BAD)/끝(GOOD) 삽입/삭제 => push_back()/pop_back()
-	// - 임의 접근 Random Access => index 접근 : 지원하는 이유 : 모든 원소가 연속적으로 저장되어 있기에 가능한
-
-	// but 중간 삽입 삭제가 불가능한 것은 아님
-	// 그렇다면 각각의 반환값은 무엇일까?
-	vector<int>::iterator insertIt = v.insert(v.begin() + 2, 5); //추가한 5의 주소
-	vector<int>::iterator eraseIt1 = v.erase(v.begin() + 2);	// 삭제한 곳의 주소 (데이터가 앞으로 당겨짐 고려)
-	vector<int>::iterator eraseIt2 = v.erase(v.begin() + 2, v.begin() + 4); // 삭제한 곳의 주소
-	
-	// 쭉- 스캔하면서 3이라는 데이터가 있으면 일괄 삭제
-	for (vector<int>::iterator it = v.begin(); it != v.end();) {
-		int data = *it;
-		if (data == 3) {
-			it = v.erase(it);
-			// crash 발생 = iterator은 주소만 가진것이 아니라 소속하는 container에 대한 정보도 가지고 있어서
-			// 그 자체를 삭제해주면 더이상 it는 무소속이 됨 -> 근데 건드리기에...
-			// 그래서 삭제해주고 그 값을 다시 it에 넣고 갱신해서 소속 유지
-			// 근데 이렇게 하면 땡겨진 새로운 값에서 ++it를 하니까 땡겨진 값은 조사도 안하고 넘어가게됨
-			// 그걸 방지하기 위해 ++it을 아래와 같이 조정해줌
-		}
-		else {
-			++it;
-		}
-	}
-	
 	
 	return 0;
 }
