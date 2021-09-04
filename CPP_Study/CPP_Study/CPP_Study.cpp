55f76f9bc7d641c433c88c58a831b321ab04dcfc
tldmschl81 <chse0810@gmail.com>
2021-09-04 19:33:05 +0900

    12-smart pointer

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,117 +3,117 @@
 
 using namespace std;
 
-// 오늘의 주제 : lambda 람다 표현식 (익명함수)
-// 사용 : [캡쳐](인자값){구협부}
-// 람다 = 함수 객체를 빠르게 만드는 문법 (새로운 기능이 들어간 것은 아님)
+// 오늘의 주제 : smart pointer
+// 간접적 포인터, 코드 안정성 유지
+// 포인터를 알맞는 정책에 따라 관리하는 객체 (포인터를 래핑해서 사용)(생명주기 자동관리)
+// shared_ptr, weak_ptr, unique_ptr
+// shared_ptr, weak_ptr 는 서로 짝꿍~
+// unique_ptr : 나만 이 포인터를 가지고 있음 -> 다른애한테 대입 X, 옮겨주고자한다면 move함수로 오른값 전달(일반적인 복사가 불허)
 
-enum class ItemType {
-	None,
-	Armor,
-	weapon,
-	Jewelry,
-	consumable
-};
+// reference count : 해당 포인터가 몇 명이나 참조하고 있나?
+// 그래서 아무도 해당 객체를 참조하지 않을 때(count == 0?) 소멸가능
 
-enum class Rarity {
-	Common,
-	Rare,
-	Unique
-};
-
-class Item {
+class Knight {
 public:
-	int _itemid = 0;
-	Rarity _rarity = Rarity::Common;
-	ItemType _type = ItemType::None;
+	Knight() { cout << "Knight 생성" << endl; }
+	~Knight() { cout << "Knight 소멸" << endl; }
 
+	void Attack() {
+		if (_target.expired() == false) { // 해당 포인터가 날라갔냐 확인, weak ptr 자체로는 타켓 그냥 사용불가
+			shared_ptr<Knight> sptr = _target.lock();
+			sptr->_hp -= _damage;
+			cout << "HP : "<< sptr->_hp << endl;
+		}
+	}
 public:
-	Item(){}
-	Item(int itemid, Rarity rarity, ItemType type) : _itemid(itemid), _rarity(rarity), _type(type) {}
-
+	int _hp = 100;
+	int _damage = 10;
+	weak_ptr<Knight> _target;// Knight* _target = nullptr;
 };
 
+class RefCountBlock {
+public:
+	int _refCount = 1;
+	int _weakCount = 1;
+};
+
+template<typename T>
+class SharedPtr {
+public:
+	SharedPtr() {}
+	SharedPtr(T* ptr) : _ptr(ptr){
+		if (_ptr != nullptr) {
+			_block = new RefCountBlock();
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	// 복사 생성자
+	SharedPtr(const SharedPtr& sptr) : _ptr(sptr._ptr), _block(sptr._block) {
+		if (_ptr != nullptr) {
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	// 복사 대입 연산자
+	void operator=(const SharedPtr& sptr) {
+		_ptr = sptr._ptr;
+		_block = sptr._block;
+		if (_ptr != nullptr) {
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+	~SharedPtr() {
+		if (_ptr != nullptr) {
+			_block->_refCount--;
+			cout << "RefCount : " << _block->_refCount << endl;
+
+			if (_block->_refCount == 0) {
+				delete _ptr;
+				// delete _block;  weak_ptr 사용시,
+				cout << "delete Data"<< endl;
+			}
+		}
+	}
+public:
+	T* _ptr = nullptr;
+	RefCountBlock* _block = nullptr;
+};
 int main()
 { 
-	vector<Item> v;
-	// push_back : 기본적으로 오른 값을 받아주고 있다 (&&) / 저번 주 복습정도,,,
-	v.push_back(Item(1, Rarity::Common, ItemType::weapon));
-	v.push_back(Item(2, Rarity::Common, ItemType::Armor));
-	v.push_back(Item(3, Rarity::Rare, ItemType::Jewelry));
-	v.push_back(Item(4, Rarity::Unique, ItemType::weapon));
+	//Knight* k1 = new Knight();
+	//Knight* k2 = new Knight();
+	//k1->_target = k2;	// target 지정
 
-	struct IsUniqueItem
+	//delete k2;
+	//k1->Attack();		// k1의 target이 nulltpr이 되지는 않음, 쓰레기값이 출력됨, 댕글링 문제(useAfterFree)에서 자유로워짐
+
+	//SharedPtr<Knight> k3(new Knight());
+	//SharedPtr<Knight> k4 = k3; // 서로 같은 객체 가리키는 중
+
+	//SharedPtr<Knight> k6;
+	//{
+	//	SharedPtr<Knight> k5(new Knight());
+	//	k6 = k5;
+	//	// 내부적으로 특정 조건을 만족하면 알아서 객체소멸
+	//}
+
+	shared_ptr<Knight> k1 = make_shared<Knight>();
 	{
-		bool operator()(Item& item) {
-			return item._rarity == Rarity::Unique;
-		}
-	};
+		shared_ptr<Knight> k2 = make_shared<Knight>();
+		k1->_target = k2;
+	}
 	
-	struct FindItemByItemId
-	{
-		// 함수객체를 이용하게 되면,
-		// 원하는 특정 데이터도 넣어서 하나의 함수만 호출하는 것이 아니라
-		// 여러 함수를 호출시킬 수 있음
-		// -> 람다로도 가능
+	k1->Attack(); // 소멸되지 않았기에 정상적 출력
+	// 주로 멀티쓰레드에서는 메모리 관리가 복잡해지기에 스마트 포인터를 활용 (거의 생포인터 사용 X)
 
-		FindItemByItemId(int itemid) : _itemid(itemid) {}
+	// 객체 서로가 서로를 가리키면 절대 각각이 소멸될일이 없음 (순환) -> 해당 부분 해결 = weak_ptr
+	//		- 생명주기에서는 자유로워짐, 객체 소멸자체에는 관여를 하지 않기 때문
 
-		bool operator()(Item& item) {
-			return item._itemid == _itemid;
-		}
-
-		int _itemid;
-	};
-
-	struct FindItem {
-
-		FindItem(int itemid, Rarity rarity, ItemType type) : _itemid(itemid), _rarity(rarity), _type(type) {}
-
-		bool operator()(Item& item) {
-			return item._itemid == _itemid && item._rarity == _rarity && item._type == _type;
-		}
-
-		int _itemid;
-		Rarity _rarity;
-		ItemType _type;
-	};
-	int itemid = 4;
-	Rarity rarity = Rarity::Unique;
-	ItemType type = ItemType::weapon;
-
-	// 자동 추론, 원하는 타입이 있을경우 위처럼 변경 가능
-	auto IsUniqueLambda = [](Item& item) -> int { return item._rarity == Rarity::Unique; };
-
-	// Closure 클로져 : 람다에 의해 만들어진 실행시점 객체
-	// 조건 이름 설정안하고 그냥 바로 때려넣기도 가능!
-	// [ ] = Capture 캡쳐 : 함수 객체 내부에 변수를 저장하는 개념과 유사
-	//		- 새로 변수를 내부에서 선언해주는게 아니라 외부에 선언되어있는 변수를 가져올 수 있음
-	//		- 사진을 찰칵 [캡쳐]하듯.. 이종의스냅샷을 찍는다고 이해
-	// 기본 캡쳐 모드 : 값(복사) 방식(=) / 참조방식(&) (위에서 만든 함수객체와 완전히 똑같은 기능 수행)
-
-	// (!) 모든 변수를 한번에 캡쳐모드를 지정하는 일은 지양 -> 어떤 값이 내부적으로 복사 혹 참조가 됐는지 모름
-	// 하나하나 지정해줘서 가독성도 높이고, 뭐가 복사 혹 참조가 됐는지 알 수 있음
-	// 참조(주소) 값을 사용한다는 의미 : 내가 사용하는 동안 해당 참조값이 유효해야함 -> 건드리면 안되기에 더욱더 조심해야함
-	// 물론 갑복사도 조심해야함 , 소멸된 (delete) 객체라면, 이상한 메모리를 건드릴수 있음.
-
-	// capture 1) 값 방식 (=)
-	auto findByItemIdLambda1 = [=](Item& item) { return item._itemid == itemid; };
-	// 값을 이후에 바꿔도 그전에 실행됐다면, 바뀐값으로 실행되지 않음, 그 자체로 복사
-	
-	// capture 2) 참조 방식 (&) : 말 그대로 변수의 주소를 가져오는 느낌,모든 변수에 &가 붙어있다고 생각
-	// 이후에 값을 바꿔줘도 주소 인식이라 바뀐 값으로 식 진행
-	auto findByItemIdLambda2 = [&](Item& item) { return item._itemid == itemid; };
-
-	// 변수마다 캡쳐 모드를 지정해서 사용 가능 : 값 방식(name), 참조 방식(&name)
-	auto findByItem = [=, &type](Item& item) {
-		return item._itemid == itemid && item._rarity == rarity && item._type == type;
-	};
-
-	auto findit1 = std::find_if(v.begin(), v.end(), IsUniqueLambda);
-	auto findit2 = std::find_if(v.begin(), v.end(), FindItemByItemId(itemid));
-
-	if (findit1 != v.end())  cout << "아이템 ID : " << findit1->_itemid << endl;
-	if (findit2 != v.end())  cout << "아이템 ID : " << findit2->_itemid << endl;
+	unique_ptr<Knight> uptr = make_unique<Knight>();
+	unique_ptr<Knight> uptr2 = std::move(uptr);
 
 	return 0;
 }
